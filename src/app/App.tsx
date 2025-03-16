c505cbbbe3c518ac7f9e4dd772c279cc0f869ada
Артур <rector96@mail.ru>
2025-03-17 02:35:38 +1000

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Текст писать тут1
         </p>
       </header>
     </div>
